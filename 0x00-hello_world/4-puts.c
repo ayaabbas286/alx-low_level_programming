89fad418aaa9c72b469d4b7f9fa38d8bf4cc83ab
ayaabbas286 <ayaabbas286@gmail.com>
2023-06-15 23:07:02 +0000

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ * return: 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
