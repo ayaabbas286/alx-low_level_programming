1bafac9f446dd62f2f5233351aa8b0adea8de2ec
ayaabbas286 <ayaabbas286@gmail.com>
2023-06-16 00:01:56 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main - Programming is like building a multilingual puzzle
- * return: 0 (success)
- */
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Progamming is like building a multilingual puzzle:");
+	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types comp
+ * Return 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("Size of char: %lu byte(s)\n", (unsigned long)
+size of 
