76eb0651604acc76a5f34b43f21008dad5772381
ayaabbas286 <ayaabbas286@gmail.com>
2023-06-20 15:41:50 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Description: print_alphabet utlizes on the _putchar function
- * void function
-*/
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
 void print_alphabet(void)
 {
 	int alpha;
