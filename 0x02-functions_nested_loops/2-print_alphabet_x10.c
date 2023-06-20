e75513ec8ffeeaf14fbaba2b5399acfa5f64942b
ayaabbas286 <ayaabbas286@gmail.com>
2023-06-20 16:09:16 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,8 @@ void print_alphabet_x10(void)
 {
 	int line, alpha;
 
-	for (line = 0; line <= 9; line)
-		for (alpha = 'a'; alpha = 'z'; alpha++)
+	for (line = 0; line <= 9; line++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
 	_putchar('\n');
 }
